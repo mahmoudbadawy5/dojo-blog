68c1a71db4480753afa9ef3264040e93acaa363c
mahmoudbadawy5 <mahmoudbadawy2011@gmail.com>
2023-03-24 17:27:29 +0200

    Handling delete

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,21 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+
+    const handleDelete = (id) => {
+        setBlogs(blogs.filter((blog) => blog.id !== id))
+    }
+    
     return ( 
-        <h1>Hello from React</h1>
+        <div className="home">
+            <BlogList title="All blogs" blogs={blogs} handleDelete={handleDelete}/>
+        </div>
      );
 }
  
