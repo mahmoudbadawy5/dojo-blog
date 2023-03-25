0eec4366adb4e1579fefc6e35eade6021859b8e8
mahmoudbadawy5 <mahmoudbadawy2011@gmail.com>
2023-03-25 03:25:35 +0200

    Finishing delete function

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     let {id} = useParams();
     let {data: blog, error, isPending} = useFetch('http://localhost:8000/blogs/'+id);
+    let history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/'+id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by: {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
